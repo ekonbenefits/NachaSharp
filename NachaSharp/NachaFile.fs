adc53ddcd9a0d7f271dd51dbbfc068b2baf047ef
jbtule <jay+code@tuley.name>
2018-03-29 18:17:41 -0500

    Refactor Match into own module

--- NachaSharp/NachaFile.fs
+++ NachaSharp/NachaFile.fs
@@ -5,29 +5,30 @@ open FSharp.Control
 open System.IO
 
 module rec NachaFile =
-    let internal (|FileHeaderMatch|_|)=
-        matchRecord FileHeaderRecord 
-    let internal (|FileControlMatch|_|)=
-        matchRecord FileControlRecord
-    let internal (|BatchHeaderMatch|_|) =
-        matchRecord BatchHeaderRecord
-    let internal (|BatchControlMatch|_|) =
-        matchRecord BatchControlRecord
-    let internal matchEntryRecord constructor batchSEC =
-        matchRecord (fun x-> constructor(batchSEC, x) :> EntryDetail)
-    let internal (|EntryMatch|_|) batchSEC = 
-        multiMatch [
-                     matchEntryRecord EntryCCD batchSEC
-                     matchEntryRecord EntryPPD batchSEC
-                     matchEntryRecord EntryWildCard batchSEC
-                   ]
-
-    let internal matchEntryAddendaRecord constructor =
-        matchRecord (fun x-> constructor(x) :> EntryAddenda)
-    let internal (|EntryAddendaMatch|_|) = 
-        multiMatch [
-                     matchEntryAddendaRecord EntryAddendaWildCard
-                   ]
+    module internal Match =
+        let (|FileHeader|_|)=
+            matchRecord FileHeaderRecord 
+        let (|FileControl|_|)=
+            matchRecord FileControlRecord
+        let (|BatchHeader|_|) =
+            matchRecord BatchHeaderRecord
+        let (|BatchControl|_|) =
+            matchRecord BatchControlRecord
+        let matchEntryRecord constructor batchSEC =
+            matchRecord (fun x-> constructor(batchSEC, x) :> EntryDetail)
+        let (|EntryDetail|_|) batchSEC = 
+            multiMatch [
+                         matchEntryRecord EntryCCD batchSEC
+                         matchEntryRecord EntryPPD batchSEC
+                         matchEntryRecord EntryWildCard batchSEC
+                       ]
+    
+        let matchEntryAddendaRecord constructor =
+            matchRecord (fun x-> constructor(x) :> EntryAddenda)
+        let (|EntryAddenda|_|) = 
+            multiMatch [
+                         matchEntryAddendaRecord EntryAddendaWildCard
+                       ]
                   
     let ParseLines lines = syncParseLines asyncParseLinesDef lines
     
@@ -43,25 +44,25 @@ module rec NachaFile =
         let! current = enumerator.MoveNext()
         while isMissingRecordButHasString head current do
             match current |> Option.get with
-                | FileHeaderMatch (fh) -> 
+                | Match.FileHeader (fh) -> 
                     head <- SomeRecord(fh)
                     let! currentFH = enumerator.MoveNext()
                     while isMissingRecordButHasString fh.FileControl currentFH do
                         match currentFH |> Option.get with
-                            | FileControlMatch t ->
+                            | Match.FileControl t ->
                                 fh.FileControl<- SomeRecord(t)
-                            | BatchHeaderMatch bh ->
+                            | Match.BatchHeader bh ->
                                 fh.Batches.Add(bh)
                                 let! currentBH = enumerator.MoveNext()
                                 while isMissingRecordButHasString bh.BatchControl currentBH do
                                     match currentBH |> Option.get with
-                                        | BatchControlMatch bt -> bh.BatchControl <- SomeRecord(bt)
-                                        | EntryMatch bh.StandardEntryClass ed ->
+                                        | Match.BatchControl bt -> bh.BatchControl <- SomeRecord(bt)
+                                        | Match.EntryDetail bh.StandardEntryClass ed ->
                                             bh.Entries.Add(ed)
                                             for _ in 0..ed.AddendaRecordedIndicator do
                                                 let! currentAdd = enumerator.MoveNext()
                                                 match currentAdd |> Option.get with
-                                                | EntryAddendaMatch add ->
+                                                | Match.EntryAddenda add ->
                                                     ed.Addenda.Add(add)
                                                 | _ -> raise <| InvalidDataException("Incorrect Addenda Indicator")
                                         | _ -> ()
