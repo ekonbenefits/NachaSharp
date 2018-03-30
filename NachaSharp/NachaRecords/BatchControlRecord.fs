4dc2dd166eeac7338841084e3c7ab4650e4c8131
jbtule <jay+code@tuley.name>
2018-03-29 23:44:19 -0500

    Added rest of meta data

--- NachaSharp/NachaRecords/BatchControlRecord.fs
+++ NachaSharp/NachaRecords/BatchControlRecord.fs
@@ -1,6 +1,7 @@
 namespace NachaSharp
 
 open FSharp.Data.FlatFileMeta
+open FSharp.Data.FlatFileMeta
 open FSharp.Data.FlatFileMeta.MetaDataHelper
 
 
@@ -10,6 +11,50 @@ type BatchControlRecord(rowInput) =
         setup this <|
                 lazy ({ 
                          columns =[
+                                        MetaColumn.Make( 1, this.RecordTypeCode, Format.leftPadString)
+                                        MetaColumn.Make( 3, this.ServiceClassCode, Format.leftPadString)
+                                        MetaColumn.Make( 6, this.Entry_AddendaCount, Format.zerodInt)
+                                        MetaColumn.Make(10, this.EntryHash, Format.zerodInt)
+                                        MetaColumn.Make(12, this.TotalDebitEntryAmount, Format.reqMoney)
+                                        MetaColumn.Make(12, this.TotalCreditEntryAmount, Format.reqMoney)
+                                        MetaColumn.Make(10, this.CompanyIdentification, Format.rightPadString)
+                                        MetaColumn.Make(19, this.MAC, Format.leftPadString)
+                                        MetaColumn.PlaceHolder(6)
+                                        MetaColumn.Make( 8, this.OriginatingDfiIdentification, Format.leftPadString)
+                                        MetaColumn.Make( 7, this.BatchNumber, Format.zerodInt)
+
                                   ]
                          length = 94
                      })
+                     
+    member this.ServiceClassCode
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value
+    member this.Entry_AddendaCount
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<int> value
+    member this.EntryHash
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<int> value            
+    member this.TotalDebitEntryAmount
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<decimal> value
+    member this.TotalCreditEntryAmount
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<decimal> value
+    member this.CompanyIdentification
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value           
+    member this.MAC
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value
+    member this.OriginatingDfiIdentification
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value
+    member this.BatchNumber
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<int> value             
+            
+            
+            
+            
--- NachaSharp/NachaRecords/EntryDetail/EntryDetail.fs
+++ NachaSharp/NachaRecords/EntryDetail/EntryDetail.fs
@@ -37,17 +37,92 @@ type EntryCCD(batchSEC, rowInput) =
         setup this <|
                 lazy ({ 
                          columns =[
+                                     MetaColumn.Make( 1, this.RecordTypeCode, Format.leftPadString)
+                                     MetaColumn.Make( 2, this.TransactionCode, Format.leftPadString)
+                                     MetaColumn.Make( 8, this.ReceivingDfiIdentification, Format.leftPadString)
+                                     MetaColumn.Make( 1, this.CheckDigit, Format.zerodInt)
+                                     MetaColumn.Make(17, this.DfiAccountNUmber, Format.leftPadString)
+                                     MetaColumn.Make(10, this.Amount, Format.reqMoney)
+                                     MetaColumn.Make(15, this.IdentificationNumber, Format.leftPadString)
+                                     MetaColumn.Make(22, this.ReceivingCompanyName, Format.leftPadString)
+                                     MetaColumn.Make( 2, this.DiscretionaryData, Format.rightPadString)
+                                     MetaColumn.Make( 1, this.AddendaRecordedIndicator, Format.zerodInt)
+                                     MetaColumn.Make(15, this.TraceNumber, Format.leftPadString)
                                   ]
                          length = 94
                      })
+    member this.TransactionCode
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value
+    member this.ReceivingDfiIdentification
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value 
+    member this.CheckDigit
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<int> value
+    member this.DfiAccountNUmber
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value 
+    member this.Amount
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<decimal> value
+    member this.IdentificationNumber
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value 
+    member this.ReceivingCompanyName
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value
+    member this.DiscretionaryData
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value
+    member this.TraceNumber
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value  
                      
 type EntryPPD(batchSEC, rowInput) =
     inherit EntryDetail("PPD", batchSEC, rowInput)
     override this.Setup () = 
         setup this <|
                 lazy ({ 
-                         columns =[
-                                  ]
+                         columns =[ 
+                                        MetaColumn.Make( 1, this.RecordTypeCode, Format.leftPadString)
+                                        MetaColumn.Make( 2, this.TransactionCode, Format.leftPadString)
+                                        MetaColumn.Make( 8, this.ReceivingDfiIdentification, Format.leftPadString)
+                                        MetaColumn.Make( 1, this.CheckDigit, Format.zerodInt)
+                                        MetaColumn.Make(17, this.DfiAccountNUmber, Format.leftPadString)
+                                        MetaColumn.Make(10, this.Amount, Format.reqMoney)
+                                        MetaColumn.Make(15, this.IndividualIdentificationNumber, Format.leftPadString)
+                                        MetaColumn.Make(22, this.IndividualName, Format.leftPadString)
+                                        MetaColumn.Make( 2, this.DiscretionaryData, Format.rightPadString)
+                                        MetaColumn.Make( 1, this.AddendaRecordedIndicator, Format.zerodInt)
+                                        MetaColumn.Make(15, this.TraceNumber, Format.leftPadString)
+                              ]
                          length = 94
                      })
-    
+    member this.TransactionCode
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value
+    member this.ReceivingDfiIdentification
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value 
+    member this.CheckDigit
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<int> value
+    member this.DfiAccountNUmber
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value 
+    member this.Amount
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<decimal> value
+    member this.IndividualIdentificationNumber
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value 
+    member this.IndividualName
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value
+    member this.DiscretionaryData
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value
+    member this.TraceNumber
+            with get () = this.GetColumn ()
+            and set value = this.SetColumn<string> value                      
