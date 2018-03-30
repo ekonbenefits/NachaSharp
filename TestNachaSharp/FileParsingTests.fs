5b79cc2af20132e7e0ddb18c6229834dac272965
jbtule <jay+code@tuley.name>
2018-03-30 12:23:37 -0500

    Added another parse test

--- TestNachaSharp/FileParsingTests.fs
+++ TestNachaSharp/FileParsingTests.fs
@@ -7,21 +7,57 @@ open FsUnit.Xunit
 open System.IO
 open System.Collections.Generic
 
-let ``Get web-debit.ach.txt`` () =
-    let path = Path.Combine(__SOURCE_DIRECTORY__,"Data", "web-debit.ach.txt")
-    use stream = File.OpenRead(path)
-    NachaFile.ParseFile(stream) |> MaybeRecord.toOption
 
+let parseFile file =
+        let path = Path.Combine(__SOURCE_DIRECTORY__,"Data", file)
+        use stream = File.OpenRead(path)
+        NachaFile.ParseFile(stream) |> MaybeRecord.toOption
+        
+let countBatches: FileHeaderRecord option -> int = 
+            Option.map (fun x-> x.Batches)
+              >> Option.defaultValue (upcast List())
+              >> Seq.length
+              
+let countEntries: FileHeaderRecord option -> int = 
+            Option.map (fun x-> x.Batches |> Seq.collect(fun y -> y.Entries))
+              >> Option.defaultValue (upcast List())
+              >> Seq.length
+              
+              
 [<Fact>]
 let ``Parse web-debit.ach.txt`` () =
-    
-    ``Get web-debit.ach.txt``() |> Option.isSome |> should equal true
+    parseFile "web-debit.ach.txt" 
+        |> Option.isSome
+        |> should equal true
     
 [<Fact>]
 let ``Parse web-debit.ach.txt Batches`` () =
-    
-    ``Get web-debit.ach.txt``() 
-        |> Option.map (fun x-> x.Batches)
-        |> Option.defaultValue (upcast List())
-        |> Seq.length
+    parseFile "web-debit.ach.txt" 
+        |> countBatches
         |> should equal 3
+        
+[<Fact>]
+let ``Parse web-debit.ach.txt Entries`` () =
+    parseFile "web-debit.ach.txt" 
+        |> countEntries
+        |> should equal 6
+        
+        
+[<Fact>]
+let ``Parse 20110805A.ach.txt`` () =
+    parseFile "20110805A.ach.txt" 
+        |> Option.isSome
+        |> should equal true
+    
+[<Fact>]
+let ``Parse 20110805A.ach.txt Batches`` () =
+    parseFile "20110805A.ach.txt" 
+        |> countBatches
+        |> should equal 4
+        
+[<Fact>]
+let ``Parse 20110805A.ach.txt Entries`` () =
+    parseFile "20110805A.ach.txt" 
+        |> countEntries
+        |> should equal 6
+        
