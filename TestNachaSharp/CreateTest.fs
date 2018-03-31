d306a22e4fc0e774599bb62863574cd239dab5cd
jbtule <jay+code@tuley.name>
2018-03-31 14:42:52 -0500

    Added some more tests

--- TestNachaSharp/CreateTest.fs
+++ TestNachaSharp/CreateTest.fs
@@ -4,6 +4,7 @@ open FSharp.Data.FlatFileMeta
 open NachaSharp
 open Xunit
 open FsUnit.Xunit
+open System
 open System.IO
 open System.Collections.Generic
 
@@ -12,3 +13,18 @@ let ``Create Blank FileHeaderRecord`` () =
     let header =  FileHeaderRecord.Create("DummyDest","DummyOrigin", "DummyID")
                                 
     header.RecordTypeCode |> should equal "1"
+    
+[<Fact>]
+let ``Create Blank Batch`` () =
+   let header =  FileHeaderRecord.Create("DummyDest","DummyOrigin", "DummyID")
+   let batch = BatchHeaderRecord.Create("Code DUM",
+                                        "Company Name DUM", 
+                                        "COMP ident DUM",
+                                        "secCode DUM",
+                                        "COMPANY ENtry dum",
+                                        DateTime(2018,1,1),
+                                        "origin status dum",
+                                        "dfi dum",
+                                        10)
+                      
+   header.RecordTypeCode |> should equal "1"
